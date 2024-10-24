4d89f79ee3dfec60888c8bcd9655ab60f47e1121
maheshj772 <maheshjadhav772@gmail.com>
2024-10-24 07:45:53 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "mahesh@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+console.log(accountCity);
+
+acccountEmail = "hc@hc.com"
+accountPassword = "1238552"
+accountCity = "benguluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"  //treat all JS code as newer version 
+
+// alert(3 + 3)  //we are using nodeJS, not browser33
+
+// number 
+
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object  
+
+console.log(typeof null);  //object
+console.log(typeof undefined);  //undefined
