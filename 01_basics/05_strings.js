28c95eb2d573cc6455ecd8f3d09af06e86ed4b8a
maheshj772 <maheshjadhav772@gmail.com>
2024-10-26 09:42:24 +0000

    basics js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,19 @@
+const name = "mahesh"
+const repoCount = 50
+
+// console.log("Hello" + name + repoCount);
+
+console.log(`my name is ${name} and my repo count is ${repoCount}`);
+
+
+const newString = "     hitesh     "
+console.log(newString);
+console.log(newString.trim());
+
+
+const url = "https://mahesh.com/mahesh%20jadhav"
+
+console.log(url.replace('%20', '_'));
+
+console.log(url.includes('mahesh'));
+
---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,36 @@
+// const score = 400
+// console.log(score);                 //400
+
+// const balance = new Number(100)
+// console.log(balance);                      //  [Number: 100]
+
+// console.log(balance.toString().length);            // 3
+// console.log(balance.toFixed(2));                   // 100.00
+
+// const otherNumber = 23.8965
+
+// console.log(otherNumber.toPrecision(3));             //23.9
+
+// const Num = 123.585
+// console.log(Num.toPrecision(3));                       // 124
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));                // 1,00,00,000
+
+
+// +++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.round(4.7));               // 5
+console.log(Math.ceil(4.2));               // 5
+console.log(Math.floor(4.9));                 // 4
+console.log(Math.max(3, 4, 8, 7));                // 8
+console.log(Math.min(3, 4, 8, 7));                 // 3
+
+
+
+
+
+
+
+
